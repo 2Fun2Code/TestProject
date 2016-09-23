4170b7ce506ad38f5d4130b27797ab14836df92f
Henrik Olsson <henrik.olsson@evry.com>
2016-09-23 13:18:58 +0200

    updated git ignore

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "ScorecardProject",
+    name: "TestProject",
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0)
     ]
